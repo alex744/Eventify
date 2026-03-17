38319829c476b0f2de33a6392aebeb3a4a723daa
Alexander Telnov <a.telnov@zenit.ru>
2026-03-17 14:35:50 +0300

    Создал стартовый проект

---
+++ Ya.Events.WebApi/Program.cs
@@ -0,0 +1,13 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Добавление сервисов в контейнер.
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Конфигурация конвейера HTTP-запросов.
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
